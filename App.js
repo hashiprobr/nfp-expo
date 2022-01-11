69bbdbecaf752f8447a7a6153097bfbbd043eb76
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-01-11 13:43:04 -0300

    Add expo-use-locator dependencies

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
+import { Platform } from 'react-native';
+
 import Main from './components/Main.js';
+
+if (Platform.OS === 'web') {
+    const warn = console.warn;
+    console.warn = (warning) => {
+        if (typeof warning !== 'string' || warning.indexOf('RCTAnimation') === -1) {
+            warn(warning);
+        }
+    }
+}
 
 export default function App() {
     return (
